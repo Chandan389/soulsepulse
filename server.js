c3f40e8ffd267a768b3f2eba31ebaf0ae264850d
Chandan389 <chandanbhagat389@gmail.com>
2025-03-13 00:12:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import fs from 'fs';
-import fetch from 'node-fetch';
-import dotenv from 'dotenv';
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const fetch = require("node-fetch");
+const dotenv = require("dotenv");
  
 dotenv.config();
  
